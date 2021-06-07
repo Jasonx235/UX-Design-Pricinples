1171c611dd0c44dbdaf2d2514c1185cae8272f3e
Jason Mui <Jasonmui22@gmail.com>
2021-06-07 18:37:21 -0400

    Added menu and selector

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,9 +2,53 @@ import React, { useState } from "react";
 
 function Content() {
   const [selected, setSelected] = useState("Affordance");
+  const menuItems = [
+    {
+      label: "Affordance",
+      action: () => {
+        setSelected("Affordance");
+      },
+    },
+    {
+      label: "Mapping",
+      action: () => {
+        setSelected("Mapping");
+      },
+    },
+    {
+      label: "Consistency",
+      action: () => {
+        setSelected("Consistency");
+      },
+    },
+  ];
+
   return (
     <div className='content-container'>
-      <div className='menu-container'>Menu</div>
+      <div className='menu-container'>
+        <div className='d-lg-none'>
+          <select onChange={(e) => setSelected(e.target.value)}>
+            {menuItems.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
+        </div>
+        <div className='d-none d-lg-block '>
+          <ul>
+            {menuItems.map((option) => {
+              return (
+                <li
+                  onClick={() => {
+                    option.action();
+                  }}
+                >
+                  {option.label}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
       <div className='information-container'>
         <p>{selected}</p>
         <div className='module-container'>
