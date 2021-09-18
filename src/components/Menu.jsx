1ed3343afe06d1290f12fa9909d2a1336d82c128
Jason Mui <Jasonmui22@gmail.com>
2021-09-18 18:17:20 -0400

    mobile menu complete

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,28 +1,38 @@
 import React, { useState } from "react";
 import menuItems from "../Data";
 import Information from "./Information";
+import Dropdown from "react-dropdown";
 
 function Menu() {
   const [selected, setSelected] = useState("About");
   const [selectedID, setSelectedID] = useState(0);
+  const options = menuItems.map((option) => ({
+    value: option,
+    key: option.id + 200,
+    label: option.name,
+  }));
 
   return (
     <div className='content-container'>
       <div className='menu-container'>
         <div className='d-lg-none'>
-          <select
+          <Dropdown
             onChange={(e) => {
-              let info = JSON.parse(e.target.value);
-              setSelected(info.name);
-              setSelectedID(info.id);
+              console.log(e.label);
+              console.log(e.value.id);
+              setSelected(e.value.name);
+              setSelectedID(e.value.id);
             }}
-          >
-            {menuItems.map((option) => (
-              <option value={JSON.stringify(option)} key={option.id}>
-                {option.name}
-              </option>
-            ))}
-          </select>
+            options={options}
+            className='drop-down'
+            placeholder='About'
+            controlClassName='myControlClassName'
+            placeholderClassName='myPlaceholderClassName'
+            menuClassName='myMenuClassName'
+            arrowClassName='myArrowClassName'
+            arrowClosed={<span className='arrow-closed'>&#9660;</span>}
+            arrowOpen={<span className='arrow-open'>&#9650;</span>}
+          ></Dropdown>
         </div>
         <div className='d-none d-lg-block'>
           <ul className='text-center list'>
@@ -33,8 +43,10 @@ function Menu() {
                     setSelected(option.name);
                     setSelectedID(option.id);
                   }}
-                  key={option.id}
-                  className={"menu " + (selected === option.name ? "selected" : "")}
+                  key={option.id + 100}
+                  className={
+                    "menu " + (selected === option.name ? "selected" : "")
+                  }
                 >
                   {option.name}
                 </li>
