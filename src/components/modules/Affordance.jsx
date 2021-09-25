3bac2ea41878616059d347a1278bd0b0a21aba61
Jason Mui <Jasonmui22@gmail.com>
2021-09-25 17:31:49 -0400

    logic for affordance no style yet

--- src/components/modules/Affordance.jsx
+++ src/components/modules/Affordance.jsx
@@ -1,19 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import airplane from "../../images/moduleAssets/affordance/airplane.png";
+import submarine from "../../images/moduleAssets/affordance/submarine.png";
 
-export default function Affordance({status}) {
-    if (status) {
-        return (
-            <div className="module yes">
-                Yes
-            </div>
-        );
-        
-    }
-    else {
-        return (
-            <div className='module no'>
-                No
-            </div>
-        );
-    }
+export default function Affordance({ status }) {
+  const [noCounter, setNoCounter] = useState(0);
+  const [yesCounter, setYesCounter] = useState(0);
+  if (status) {
+    return (
+      <div className='module no'>
+        <p>WITHOUT AFFORDANCE</p>
+        <div>
+          <img
+            src={airplane}
+            alt='airplane'
+            onClick={() => {
+              setNoCounter(noCounter + 1);
+            }}
+          />
+          <div>
+            <p>{noCounter}</p>
+            <p
+              onClick={() => {
+                setNoCounter(0);
+              }}
+            >
+              Reset
+            </p>
+          </div>
+          <img
+            src={submarine}
+            alt='submarine'
+            onClick={() => {
+              setNoCounter(noCounter - 1);
+            }}
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className='module yes'>
+        <p>WITH AFFORDANCE</p>
+        <div>
+          <span
+            onClick={() => {
+              setNoCounter(noCounter - 1);
+            }}
+          >
+            -
+          </span>
+          <span>{noCounter}</span>
+          <span
+            onClick={() => {
+              setNoCounter(noCounter + 1);
+            }}
+          >
+            +
+          </span>
+        </div>
+        <div
+          onClick={() => {
+            setNoCounter(0);
+          }}
+        >
+          Reset
+        </div>
+      </div>
+    );
+  }
 }
