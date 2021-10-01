45b44cb6b3903b56f68827691fb9eb029b7cee3e
Jason Mui <Jasonmui22@gmail.com>
2021-10-01 16:50:56 -0400

    constraint module setup

--- src/components/modules/Constraint.jsx
+++ src/components/modules/Constraint.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Constraint({ status }) {
+  const [myData, setMyData] = useState("");
+
   if (status) {
     return (
-      <div className='module no'>
+      <div className='module no constraint'>
         <p className='title'>WITHOUT CONSTRAINT</p>
+        <div className='constraint-container'>
+          <label htmlFor='name'>Enter your name</label>
+          <input
+            type='text'
+            name='name'
+            onChange={(e) => {
+              setMyData(e.target.value);
+              console.log(myData);
+            }}
+          />
+        </div>
+        <input type='submit' name='submit' id='submit' />
       </div>
     );
   } else {
     return (
-      <div className='module yes'>
+      <div className='module yes constraint'>
         <p className='title'>WITH CONSTRAINT</p>
+        <div className='constraint-container'>
+          <label htmlFor='name'>Enter your name</label>
+          <input
+            type='text'
+            name='name'
+            onChange={(e) => {
+              setMyData(e.target.value);
+              console.log(myData);
+            }}
+          />
+        </div>
+        <input type='submit' name='submit' id='submit' />
       </div>
     );
   }
