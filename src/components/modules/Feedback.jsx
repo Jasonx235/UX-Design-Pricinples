f3a86af5c9abe7ec87054d88eb486ee70f7a1606
Jason Mui <Jasonmui22@gmail.com>
2021-10-09 17:09:55 -0400

    feedback html complete

--- src/components/modules/Feedback.jsx
+++ src/components/modules/Feedback.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Feedback({ status }) {
   if (status) {
     return (
       <div className='module no'>
         <p className='title'>WITHOUT FEEDBACK</p>
+        <div>
+          <button type='submit'>OK</button>
+        </div>
       </div>
     );
   } else {
     return (
       <div className='module yes'>
         <p className='title'>WITH FEEDBACK</p>
+        <div>
+          <button type='submit'>OK</button>
+        </div>
       </div>
     );
   }
