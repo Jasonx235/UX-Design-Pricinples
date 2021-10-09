eebd24da7bc7f8035be2eaa13afa2c111050ac7f
Jason Mui <Jasonmui22@gmail.com>
2021-10-09 17:09:05 -0400

    mapping and visibilty html complete

--- src/components/modules/Mapping.jsx
+++ src/components/modules/Mapping.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React from "react";
+import lightbulbOff from "../../images/moduleAssets/mapping/akar-icons_light-bulb.png";
+import lightbulbOn from "../../images/moduleAssets/mapping/lightbulb-on.png";
+import switchOff from "../../images/moduleAssets/mapping/light-off.png";
+import switchOn from "../../images/moduleAssets/mapping/light-on.png";
 
 export default function Mapping({ status }) {
   if (status) {
     return (
       <div className='module no'>
         <p className='title'>WITHOUT MAPPING</p>
+        <div>
+          <img src={lightbulbOff} alt='lightbulb off' />
+          <img src={lightbulbOff} alt='lightbulb off' />
+          <img src={lightbulbOff} alt='lightbulb off' />
+        </div>
+        <div>
+          <img src={switchOff} alt='switch off' />
+          <img src={switchOff} alt='switch off' />
+          <img src={switchOff} alt='switch off' />
+        </div>
       </div>
     );
   } else {
     return (
       <div className='module yes'>
         <p className='title'>WITH MAPPING</p>
+        <div>
+          <img src={lightbulbOn} alt='lightbulb on' />
+          <img src={lightbulbOn} alt='lightbulb on' />
+          <img src={lightbulbOn} alt='lightbulb on ' />
+        </div>
+        <div>
+          <img src={switchOn} alt='switch on' />
+          <img src={switchOn} alt='switch on' />
+          <img src={switchOn} alt='switch on' />
+        </div>
       </div>
     );
   }
--- src/components/modules/Visibility.jsx
+++ src/components/modules/Visibility.jsx
@@ -5,12 +5,18 @@ export default function Visibility({ status }) {
     return (
       <div className='module no'>
         <p className='title'>WITHOUT VISIBILITY</p>
+        <p>Welcome!</p>
+        <p>All items 50% Off</p>
+        <p>Click here to Enter</p>
       </div>
     );
   } else {
     return (
       <div className='module yes'>
         <p className='title'>WITH VISIBILITY</p>
+        <p>Welcome!</p>
+        <p>All items 50% Off</p>
+        <button type='submit'>Enter -></button>
       </div>
     );
   }
