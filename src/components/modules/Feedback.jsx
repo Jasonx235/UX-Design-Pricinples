a3cc502712e005fc18404dd0fdf6a075a1d8db47
Jason Mui <Jasonmui22@gmail.com>
2021-10-11 17:58:24 -0400

    feedback logic complete needs styling

--- src/components/modules/Feedback.jsx
+++ src/components/modules/Feedback.jsx
@@ -1,22 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactLoading from "react-loading";
 
 export default function Feedback({ status }) {
+  const [submitted, setSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmitYes = () => {
+    setLoading(true);
+    setSubmitted(true);
+    setTimeout(() => setLoading(false), 1500);
+  };
+
+  const handleSubmitNo = () => {
+    setTimeout(() => setSubmitted(true), 1500);
+  };
   if (status) {
     return (
-      <div className='module no'>
-        <p className='title'>WITHOUT FEEDBACK</p>
-        <div>
-          <button type='submit'>OK</button>
-        </div>
+      <div className='module no feedback'>
+        {submitted ? (
+          <div>
+            hi
+            <div className='close-button'>
+              <button
+                onClick={() => {
+                  setSubmitted(false);
+                }}
+              >
+                X
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className='feedback-container'>
+            <p className='title'>WITHOUT FEEDBACK</p>
+            <div className='button-container'>
+              <input
+                type='submit'
+                value='OK'
+                className='feedbackNo'
+                onClick={() => {
+                  handleSubmitNo();
+                }}
+              />
+            </div>
+          </div>
+        )}
       </div>
     );
   } else {
     return (
-      <div className='module yes'>
-        <p className='title'>WITH FEEDBACK</p>
-        <div>
-          <button type='submit'>OK</button>
-        </div>
+      <div className='module yes feedback'>
+        {loading ? (
+          <div className='center'>
+            <ReactLoading type='spin' color='#43b856' />
+          </div>
+        ) : submitted ? (
+          <div>
+            hi
+            <div className='close-button'>
+              <button
+                onClick={() => {
+                  setSubmitted(false);
+                }}
+              >
+                X
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className='feedback-container'>
+            <p className='title'>WITH FEEDBACK</p>
+            <div className='button-container'>
+              <input
+                type='submit'
+                value='OK'
+                className='feedbackYes'
+                onClick={() => {
+                  handleSubmitYes();
+                }}
+              />
+            </div>
+          </div>
+        )}
       </div>
     );
   }
